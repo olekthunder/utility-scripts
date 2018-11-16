d5b2d6cce53047d8ec403b7b05d631948f577254
Oleksandr Zinkevych <a.zinkevych@smartweb.com.ua>
2018-11-16 17:53:14 +0200

    Initial

---
+++ run_in_new_tab.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+TERMINAL="gnome-terminal"
+
+pgrep -u "$USER" gnome-terminal
+# if last command is unsuccessful
+if [ "$?" -ne 0 ]; then
+    /usr/bin/$TERMINAL
+    sleep 2
+    WID=`xdotool search --class "$TERMINAL" | head -1`
+    # Runs command given as first argument in new tab of terminal
+    xdotool windowfocus $WID
+    sleep .1
+fi
+
+
+run_in_new_tab() {
+    # Runs command given as first argument in new tab of terminal
+    xdotool key ctrl+shift+t
+    sleep .2
+    for arg in "$@"
+    do
+        xdotool type --delay .1 "$arg"
+        xdotool key Return
+        sleep .2
+    done
+}
+
+run_in_new_tab "echo lol" "echo keke"
+
