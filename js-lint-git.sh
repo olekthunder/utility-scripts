9c7a6b615b9dcec2646c435b5930bf65c2068a1c
Oleksandr Zinkevych <a.zinkevych@smartweb.com.ua>
2018-11-16 20:56:29 +0200

    js-lint-git: added npm repo check

--- js-lint-git.sh
+++ js-lint-git.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
-for i in $(git diff --name-only $1 \*.js \*.jsx)
-do
-    npm run eslint "$i"
-done
+
+check_installed() {
+    for arg in "$@"
+    do
+        if [ ! "$(which "$arg")" ]; then
+            echo -e "\e[41mERROR!\e[49m \e[1m$arg\e[21m is not installed. Install it via your system package manager"
+            exit 1
+        fi
+    done
+}
+
+
+check_inside_npm_repository() {
+    if [ ! -f "package.json" ]; then
+        echo -e "\e[41mERROR!\e[49m Not a npm repository!"
+        exit 1
+    fi
+}
+
+
+main() {
+    for i in $(git diff --name-only $1 \*.js \*.jsx)
+    do
+        npm run eslint "$i"
+    done
+}
+
+
+check_inside_npm_repository
+check_installed "eslint"
 
