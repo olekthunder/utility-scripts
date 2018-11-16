a9c81efce53160b830f38d52487717a0d7799276
Oleksandr Zinkevych <a.zinkevych@smartweb.com.ua>
2018-11-16 18:20:03 +0200

    Check for needed packages

--- run_in_new_tab.sh
+++ run_in_new_tab.sh
@@ -2,15 +2,16 @@
 
 TERMINAL="gnome-terminal"
 
-# if last command is unsuccessful
-if [ pgrep -u "$USER" "$TERMINAL" ]; then
-    /usr/bin/$TERMINAL
-    sleep 2
-    WID=`xdotool search --class "$TERMINAL" | head -1`
-    # Runs command given as first argument in new tab of terminal
-    xdotool windowfocus $WID
-    sleep .1
-fi
+
+check_installed() {
+    for arg in "$@"
+    do
+        if [ ! "$(which "$arg")" ]; then
+            echo -e "\e[41mERROR!\e[49m \e[1m$arg\e[21m is not installed. Install it via your system package manager"
+            exit 1
+        fi
+    done
+}
 
 
 run_in_new_tab() {
@@ -25,5 +26,20 @@ run_in_new_tab() {
     done
 }
 
-run_in_new_tab "echo lol" "echo keke"
+
+main() {
+    if [ pgrep -u "$USER" "$TERMINAL" ]; then
+        /usr/bin/$TERMINAL
+        sleep 2
+        WID=`xdotool search --class "$TERMINAL" | head -1`
+        # Runs command given as first argument in new tab of terminal
+        xdotool windowfocus $WID
+        sleep .1
+    fi
+    run_in_new_tab "echo lol" "echo keke"
+}
+
+
+check_installed "xdotool"
+main
 
