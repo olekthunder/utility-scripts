1761fab26f52af6971f54f16dfb65fdead8d12ae
Oleksandr Zinkevych <a.zinkevych@smartweb.com.ua>
2019-01-16 12:04:05 +0200

    Add reset_idea.sh

---
+++ reset_idea.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+## declare array of tools
+declare -a tools=(
+    "DataGrip"
+    "CLion"
+    "Rider"
+    "PhpStorm"
+    "PyCharm"
+    )
+
+for tool in "${tools[@]}"
+do
+    echo "removing evaluation key for $tool"
+    rm -rf ~/.$tool*/config/eval
+    rm -rf ~/.java/.userPrefs/jetbrains/${tool,,}
+done
+
+for tool in "${tools[@]}"
+do
+    echo "resetting evalsprt in options.xml for $tool"
+    sed -i '/evlsprt/d' ~/.$tool*/config/options/options.xml
+done
+
+echo "resetting evalsprt in prefs.xml"
+sed -i '/evlsprt/d' ~/.java/.userPrefs/prefs.xml
+
+for tool in "${tools[@]}"
+do
+    echo "change date file for $tool"
+    find ~/.$tool* -type d -exec touch -t $(date +"%Y%m%d%H%M") {} +;
+    find ~/.$tool* -type f -exec touch -t $(date +"%Y%m%d%H%M") {} +;
+done
